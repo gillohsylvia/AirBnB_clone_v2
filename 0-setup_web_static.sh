dfbe0b555d4ccb02ab29ab972f39e3aaa166ce13
Isquare007 <akibrahiim2019@gmail.com>
2022-12-22 08:44:01 +0000

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,36 +6,14 @@ sudo apt-get update -y
 sudo apt-get install nginx -y
 
 #create neccessary folders
-if [ -d "/data/web_static/shared/" ]
-then
-	echo "/data/web_static/shared/ already exist"
-else
-	sudo mkdir -p '/data/web_static/shared/'
-fi
-
-if [ -d "/data/web_static/releases/test" ]
-then
-        echo "/data/web_static/releases/test already exist"
-else
-        sudo mkdir -p '/data/web_static/releases/test'
-fi
-
-if [ -f "/data/web_static/releases/test/index.html" ]
-then
-        echo "/data/web_static/releases/test/index.html already exist"
-else
-        sudo touch '/data/web_static/releases/test/index.html'
-fi
+sudo mkdir -p '/data/web_static/shared/'
+sudo mkdir -p '/data/web_static/releases/test'
+sudo touch '/data/web_static/releases/test/index.html'
 
 # index.html message
 sudo echo "Welcome, this is $(hostname) server" |sudo tee /data/web_static/releases/test/index.html
 
 # creates symbolic link
-#if [ -d "/data/web_static/current" ]
-#then
-#	sudo rm -rf "/data/web_static/current already exist"
-#fi
-
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # ownership
