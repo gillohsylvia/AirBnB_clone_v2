7d1994dd4eadd0c0a561a60ad3a12e3e3af45a77
Isquare007 <akibrahiim2019@gmail.com>
2023-01-16 10:51:28 +0000

    hbnh

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,12 @@ def hello_route():
     """function for web app route"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
     """function for hbnb route"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
