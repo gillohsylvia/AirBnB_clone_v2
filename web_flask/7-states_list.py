15da4a8418f40f242c8c0bfaab2d9814d382c24f
Isquare007 <akibrahiim2019@gmail.com>
2023-01-16 11:11:46 +0000

    7 states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,22 +5,18 @@ from flask import Flask
 from flask import render_template
 from models import storage
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-"""
-@app.teardown_appcontext
-def remove_session(self):
-    storage.close()
-"""
+#app.url_map.strict_slashes = False
 
-
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes = False)
 def state_list():
+    """render list of states"""
     states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def remove_session(self):
+    """closes storage"""
     storage.close()
 
 
