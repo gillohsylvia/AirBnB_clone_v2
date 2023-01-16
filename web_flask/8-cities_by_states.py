1e3a5e2b9cf7cd26272d98827a1e15d6d7b4d8bf
Isquare007 <akibrahiim2019@gmail.com>
2023-01-16 14:35:52 +0000

    8 cities by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,10 +6,9 @@ from flask import render_template
 from models import storage
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/cities_by_states')
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_list():
     states = storage.all('State')
     cities = storage.all('City')
