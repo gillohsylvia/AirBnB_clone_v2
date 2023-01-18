adb1f9c41a8633071a2ebd9974717fb4b2634101
gillohsylvia <abdallahfatima363@gmail.com>
2023-01-19 00:18:56 +0300

    python files

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,5 +11,11 @@ def hello_hbnb():
     """ Function called with / route """
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function called with /hbnb route """
+    return 'HBNB'
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -11,5 +11,17 @@ def hello_hbnb():
     """ Function called with / route """
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function called with /hbnb route """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Function called with /c/<text> route """
+    return 'C %s' % text.replace('_', ' ')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
